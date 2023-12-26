38508f970682510375eeaf853919176e4a043c48
Tom Österlund <tom.osterlund1@gmail.com>
2023-12-26 12:57:26 +0100

    fix broken import in test

--- cypress/pages/001-smoke/001-smoke.tsx
+++ cypress/pages/001-smoke/001-smoke.tsx
@@ -5,7 +5,7 @@ import '@fontsource/open-sans/700.css'
 import '@fontsource/open-sans/700-italic.css'
 import ReactDOM from 'react-dom/client'
 import React from 'react'
-import { Calendar, useCalendarApp } from '../../../src'
+import { Calendar, useCalendarApp } from '../../..'
 import {
   viewDay,
   viewMonthAgenda,
