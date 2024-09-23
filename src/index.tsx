4699f0baf22809d6a7f61674d9e7794d212f7984
Tom Österlund <tom.osterlund1@gmail.com>
2024-09-23 07:19:43 +0200

    feat: bump schedule-x deps to v2
    BREAKING CHANGE: v1 plugins do not work with v2
    BREAKING CHANGE: Calendar alias component deprecated, use ScheduleXCalendar instead

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,2 @@
-// @deprecated. To be removed in v1.0.0
-export { ScheduleXCalendar as Calendar } from './schedule-x-calendar.tsx'
-
 export { ScheduleXCalendar } from './schedule-x-calendar.tsx'
 export { useCalendarApp, useNextCalendarApp } from './use-calendar-app.tsx'
