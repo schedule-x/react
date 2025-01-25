0bf2a7afc3398acd46223f0964245f98de14266d
Tom Österlund <tom.osterlund1@gmail.com>
2025-01-25 14:19:44 +0100

    feat: usePlugin hook

--- src/use-plugin.ts
+++ src/use-plugin.ts
@@ -4,13 +4,15 @@ import { useEffect, useState } from 'react'
 export const usePlugin = <Plugin extends PluginBase<string>>(
   plugin: Plugin
 ) => {
+  if ('undefined' === typeof window) {
+    throw new Error('usePlugin can only be used in the browser. You need to wrap all logic for the calendar in a client side component.')
+  }
+
   const [pluginInstance, setPluginInstance] = useState<Plugin>()
 
   useEffect(() => {
-    if (typeof window !== 'undefined') {
-      setPluginInstance(plugin)
-    }
+    setPluginInstance(plugin)
   }, [])
 
-  return pluginInstance
+  return pluginInstance as Plugin
 }
