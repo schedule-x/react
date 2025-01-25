beef04370a4a3b9c35a85e3b0b88217452cbe2ba
Tom Österlund <tom.osterlund1@gmail.com>
2025-01-25 14:07:30 +0100

    feat: usePlugin hook

--- src/index.tsx
+++ src/index.tsx
@@ -1,2 +1,3 @@
 export { ScheduleXCalendar } from './schedule-x-calendar.tsx'
 export { useCalendarApp, useNextCalendarApp } from './use-calendar-app.tsx'
+export { usePlugin } from "./use-plugin.ts";
