dbf7f76b982fd283601f16a46840c07e6acfd332
Tom Österlund <tom.osterlund1@gmail.com>
2025-01-25 14:20:24 +0100

    feat: usePlugin hook

--- src/use-plugin.ts
+++ src/use-plugin.ts
@@ -5,7 +5,9 @@ export const usePlugin = <Plugin extends PluginBase<string>>(
   plugin: Plugin
 ) => {
   if ('undefined' === typeof window) {
-    throw new Error('usePlugin can only be used in the browser. You need to wrap all logic for the calendar in a client side component.')
+    throw new Error(
+      'usePlugin can only be used in the browser. You need to wrap all logic for the calendar in a client side component.'
+    )
   }
 
   const [pluginInstance, setPluginInstance] = useState<Plugin>()
