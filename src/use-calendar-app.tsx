73b03718b36149445902209f4c54154d4058b08a
Bryan <31880091+raizdev@users.noreply.github.com>
2025-03-06 18:20:52 +0100

    fix: reactivity fix for react 19 (#243)
    Co-authored-by: Bryan van der Starre <bryan@starre.dev>

--- src/use-calendar-app.tsx
+++ src/use-calendar-app.tsx
@@ -1,17 +1,18 @@
-import {
-  CalendarApp,
-  CalendarConfig,
-  createCalendar,
-} from '@schedule-x/calendar'
-import { useEffect, useState } from 'react'
-import { PluginBase } from '@schedule-x/shared'
+import { useState, useEffect } from "react";
+import { CalendarApp, CalendarConfig, createCalendar } from "@schedule-x/calendar";
+import { PluginBase } from "@schedule-x/shared";
 
 export function useCalendarApp<Plugins extends PluginBase<string>[]>(
   config: CalendarConfig,
   plugins?: Plugins
 ) {
-  const [calendarApp] = useState(() => createCalendar<Plugins>(config, plugins))
-  return calendarApp
+  const [calendarApp, setCalendarApp] = useState<CalendarApp | null>(null);
+
+  useEffect(() => {
+    setCalendarApp(createCalendar<Plugins>(config, plugins));
+  }, []); 
+
+  return calendarApp;
 }
 
 export function useNextCalendarApp<Plugins extends PluginBase<string>[]>(
